61b3b84f30ef68069671f2ec8092933601513026
Kyle M Hall <kyle@bywatersolutions.com>
2020-09-18 08:07:35 -0400

    Update print status via API, delete PDF after printing

--- networkclient.cpp
+++ networkclient.cpp
@@ -77,7 +77,7 @@ void NetworkClient::downloadPrintFile(QJsonObject job) {
     QString print_file_id = QString(QString::number(job["print_file_id"].toInt()));
     qDebug() << "Print File ID: " << print_file_id;
 
-    QUrl url = QUrl("http://192.168.56.1:3000/api/printmanager/v1_0/get_file/" + print_file_id ); //TODO: Move address and port to config file
+    QUrl url = QUrl("http://10.0.2.2:3000/api/printmanager/v1_0/get_file/" + print_file_id ); //TODO: Move address and port to config file
     qDebug() << "PDF URL: " << url.toString();
     QNetworkReply* reply = nam->get(QNetworkRequest(url));
     reply->setProperty("job", job);
@@ -90,24 +90,26 @@ void NetworkClient::downloadPrintFileFinished(QNetworkReply *reply) {
     qDebug() << "JOB: " << QJsonDocument(job).toJson(QJsonDocument::Compact).toStdString().c_str();
     qDebug() << "PRINT FILE ID: " << QString::number( job["job_id"].toInt() );
 
-    //QString fileId = reply->rawHeader("File-Id");
-    QString fileId = QString::number( job["job_id"].toInt() );
-    qDebug() << "FILE ID: " + fileId;
+    //QString jobId = reply->rawHeader("File-Id");
+    QString jobId = QString::number( job["job_id"].toInt() );
+    qDebug() << "FILE ID: " + jobId;
+
+    QNetworkAccessManager nam;
+    nam.get(QNetworkRequest(QUrl("http://10.0.2.2:3000/api/printmanager/v1_0/job/" + jobId + "/InProgress" )));
 
     QString tempDir = QDir::tempPath();
     qDebug() << "Temp Dir: " << tempDir;
 
-    QString tempFile = tempDir + "/" + fileId + ".pdf";
+    QString tempFile = tempDir + "/" + jobId + ".pdf";
     QFile localFile( tempFile );
     if (!localFile.open(QIODevice::WriteOnly))
         return;
     localFile.write(reply->readAll());
     localFile.close();
 
-//  QProcess::startDetached("C:\\SumatraPDF.exe -print-to BrotherLaser " + tempFile);
     QProcess sumatra;
     qDebug() << "PRINTING TO START";
-    QString command = "C:\\SumatraPDF.exe -silent -print-to BrotherLaser " + tempFile;
+    QString command = "C:\\SumatraPDF.exe -silent -print-to BrotherLaser1 " + tempFile;
     qDebug() << "PRINT COMMAND: " << command;
     sumatra.start(command);
     sumatra.waitForStarted();
@@ -117,16 +119,20 @@ void NetworkClient::downloadPrintFileFinished(QNetworkReply *reply) {
     sumatra.waitForFinished();
     qDebug() << "PRINTING DONE!";
 
-    if ( sumatra.exitStatus() == QProcess::NormalExit ) {
-        qDebug() << "EXIT STATUS: " << sumatra.exitCode();
-        if ( sumatra.exitCode() == 0 ) {
-            qDebug() << "PRINTING " << tempFile << " SUCEEDED!";
-        }
+    qDebug() << "EXIT STATUS: " << sumatra.exitCode();
+    if ( sumatra.exitStatus() == QProcess::NormalExit && sumatra.exitCode() == 0 ) {
+        qDebug() << "PRINTING " << tempFile << " SUCEEDED!";
+        nam.get(QNetworkRequest(QUrl("http://10.0.2.2:3000/api/printmanager/v1_0/job/" + jobId + "/Done" )));
     } else {
         qDebug() << "PRINTING " << tempFile << " FAILED!";
+        nam.get(QNetworkRequest(QUrl("http://10.0.2.2:3000/api/printmanager/v1_0/job/" + jobId + "/Error" )));
     }
 
     reply->deleteLater();
+
+    QFile file(tempFile);
+    file.remove();
+
     qDebug() << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 }
 
