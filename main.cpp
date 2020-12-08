83c0b454683b45a152dd1b9c85d81c89cdfebc85
Kyle M Hall <kyle@bywatersolutions.com>
2020-12-08 07:11:59 -0500

    Redirect all log messages to a logfile

--- main.cpp
+++ main.cpp
@@ -3,11 +3,47 @@
 
 #include <QApplication>
 #include <QSettings>
+#include <QFile>
+#include <QTextStream>
+
+void messageHandler( QtMsgType type, const QMessageLogContext &context, const QString &message ) {
+    QString levelText;
+    switch(type) {
+        case QtDebugMsg:
+            levelText = "Debug";
+            break;
+        case QtInfoMsg:
+            levelText = "Info";
+            break;
+        case QtWarningMsg:
+            levelText = "Warning";
+            break;
+        case QtCriticalMsg:
+            levelText = "Critical";
+            break;
+        case QtFatalMsg:
+            levelText = "Fatal";
+            break;
+    }
+
+    QString text = QString("%3 [%1] %2")
+        .arg(levelText)
+        .arg(message)
+        .arg(QDateTime::currentDateTime().toString(Qt::ISODate));
+
+    QFile file("printmanager.log");
+    file.open(QIODevice::WriteOnly | QIODevice::Append);
+
+    QTextStream textStream(&file);
+    textStream << text << endl;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
+
+    qInstallMessageHandler(messageHandler);
 
     QCoreApplication::setOrganizationName("Libki");
     QCoreApplication::setOrganizationDomain("libki.org");
